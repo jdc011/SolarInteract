edf314351e5e034003b554ea605db387d0e4fa2d
jeremy <jdc011@ucsd.edu>
2017-02-17 16:00:50 -0800

    house keeping

--- PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/MapsActivity.java
+++ PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/MapsActivity.java
@@ -26,13 +26,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-<<<<<<< HEAD
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(33, -3);
-=======
+
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
->>>>>>> 4941f8b7de62bc9989ce2e7523da0a21f268ccfc
+        LatLng test = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
