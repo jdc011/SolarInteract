447417a8d5c9023938b94d0a3e9ccbd5d780577b
jeremy <jdc011@ucsd.edu>
2017-02-18 18:15:28 -0800

    added documentation/ house keeping

--- PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/MapsActivity.java
+++ PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/MapsActivity.java
@@ -2,26 +2,19 @@ package com.example.jeremy.pcmap;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.widget.ZoomControls;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLngBounds;
-import android.view.View;
-
-
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private final static int LOCATION = 101;
-    ZoomControls zoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,37 +23,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        /*zoom = (ZoomControls) findViewById(R.id.);
-        zoom.setOnZoomOutClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mMap.animateCamera(CameraUpdateFactory.zoomOut());
-            }
-        });
-        zoom.setOnZoomInClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mMap.animateCamera(CameraUpdateFactory.zoomIn());
-            }
-        });*/
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        // Zoom buttons
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(32.879524, -117.236522);
+        // Add a marker in PC and move the camera
+        LatLng PC = new LatLng(32.879524, -117.236522);
         GroundOverlayOptions PCOverlay = new GroundOverlayOptions()
                 .image(BitmapDescriptorFactory.fromResource(R.drawable.pclevel1))
-                .positionFromBounds(new LatLngBounds(new LatLng(32.879202, -117.237710), new LatLng(32.880276, -117.235693)));
+                .positionFromBounds(new LatLngBounds(new LatLng(32.879133, -117.237381), new LatLng(32.880180, -117.235593)));
 
+        // Add PC floor plan to map
         mMap.addGroundOverlay(PCOverlay);
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in PC"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 21.0f));
+        // Add a marker in PC, UCSD, and move the camera.
+        mMap.addMarker(new MarkerOptions().position(PC).title("Marker in PC"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(PC, 21.0f));
     }
 }
