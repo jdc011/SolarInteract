a85517e808bdc7d546591c1da845903c19fae599
jeremy <jdc011@ucsd.edu>
2017-03-02 00:22:35 -0800

    UI has buttons (buggy) and text field

--- PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/MapsActivity.java
+++ PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/MapsActivity.java
@@ -1,16 +1,16 @@
 package com.example.jeremy.pcmap;
 
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.GroundOverlayOptions;
-import com.google.android.gms.maps.model.LatLngBounds;
+        import android.os.Bundle;
+        import android.support.v4.app.FragmentActivity;
+        import com.google.android.gms.maps.CameraUpdateFactory;
+        import com.google.android.gms.maps.GoogleMap;
+        import com.google.android.gms.maps.OnMapReadyCallback;
+        import com.google.android.gms.maps.SupportMapFragment;
+        import com.google.android.gms.maps.model.LatLng;
+        import com.google.android.gms.maps.model.MarkerOptions;
+        import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+        import com.google.android.gms.maps.model.GroundOverlayOptions;
+        import com.google.android.gms.maps.model.LatLngBounds;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -19,9 +19,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.maps_activity);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
     }
 
@@ -33,10 +33,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
         // Add a marker in PC and move the camera
-        LatLng SRC = new LatLng(32.879795, -117.237141);
+        LatLng SRC = new LatLng(32.879832, -117.23723);
         GroundOverlayOptions PCOverlay = new GroundOverlayOptions()
                 .image(BitmapDescriptorFactory.fromResource(R.drawable.pclevel1))
-                .positionFromBounds(new LatLngBounds(new LatLng(32.878983, -117.237631), new LatLng(32.880461, -117.235343)));
+                .positionFromBounds(new LatLngBounds(new LatLng(32.878933, -117.237631), new LatLng(32.880461, -117.235343)));
 
         // Add PC floor plan to map
         mMap.addGroundOverlay(PCOverlay);
