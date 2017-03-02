357be9a868ae196658b675cf4ca11010ff53fc8f
jeremy <jdc011@ucsd.edu>
2017-03-02 00:38:34 -0800

    Added class of constants

---
+++ PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/Constants.java
@@ -0,0 +1,16 @@
+package com.example.jeremy.pcmap;
+
+/**
+ * Created by jeremy on 3/2/17.
+ * Contains all constant values.
+ */
+
+public class Constants {
+    public final double X_SRC = 32.879832;      //SRC x coordinate
+    public final double Y_SRC = -117.23723;     // SRC y coordinate
+    public final double X1_POS = 32.878933;     // SRC x lower corner
+    public final double X2_POS = 32.880461;     // SRC y lower corner
+    public final double Y1_POS = -117.237631;   // SRC x upper corner
+    public final double Y2_POS = -117.235343;   // SRC Y upper corner
+    public final float DEF_ZOOM = 21f;          // start zoom
+}
--- PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/MapsActivity.java
+++ PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/MapsActivity.java
@@ -14,9 +14,11 @@ package com.example.jeremy.pcmap;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    // google map API
     private GoogleMap mMap;
 
     @Override
+    // display the app page
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maps_activity);
@@ -26,23 +28,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
+    // display components in map
     public void onMapReady(GoogleMap googleMap) {
+
+        // initialize google map API
         mMap = googleMap;
+
+        Constants con = new Constants();
 
         // Zoom buttons
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
         // Add a marker in PC and move the camera
-        LatLng SRC = new LatLng(32.879832, -117.23723);
+        LatLng SRC = new LatLng(con.X_SRC, con.Y_SRC);
         GroundOverlayOptions PCOverlay = new GroundOverlayOptions()
-                .image(BitmapDescriptorFactory.fromResource(R.drawable.pclevel1))
-                .positionFromBounds(new LatLngBounds(new LatLng(32.878933, -117.237631), new LatLng(32.880461, -117.235343)));
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.pclvl1))
+                .positionFromBounds(new LatLngBounds(new LatLng(con.X1_POS, con.Y1_POS), new LatLng(con.X2_POS, con.Y2_POS)));
 
         // Add PC floor plan to map
         mMap.addGroundOverlay(PCOverlay);
 
         // Add a marker in PC, UCSD, and move the camera.
         mMap.addMarker(new MarkerOptions().position(SRC).title("You are here!"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SRC, 21.0f));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SRC, con.DEF_ZOOM));
     }
 }
