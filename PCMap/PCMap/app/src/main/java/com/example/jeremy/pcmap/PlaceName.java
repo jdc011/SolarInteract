978bcdd5f73a4d9b5ad9fd3e44837fff6e74be4c
Ajay Singh <singh.ajay0124@gmail.com>
2017-04-03 18:53:29 -0700

    commit

---
+++ PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/PlaceName.java
@@ -0,0 +1,12 @@
+package com.example.jeremy.pcmap;
+
+/**
+ * Created by ladislavsteffko on 3/7/17.
+ */
+
+// Class that enumerates names of places in PC
+public enum PlaceName {
+    SRC,
+    Santorini,
+    SunshineMarket
+}
