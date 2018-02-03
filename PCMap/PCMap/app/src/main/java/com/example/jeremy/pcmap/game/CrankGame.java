585db5ea157899d7b8896097c3ea05ede08f903a
jdc011 <jdc011@ucsd.edu>
2018-02-03 12:34:28 -0800

    Added graphview dependencies for game interface

--- PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/game/CrankGame.java
+++ PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/game/CrankGame.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.jjoe64.graphview.GraphView;
 
 import com.example.jeremy.pcmap.R;
 
