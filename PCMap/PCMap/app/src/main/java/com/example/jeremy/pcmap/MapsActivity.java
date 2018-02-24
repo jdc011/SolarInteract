69673966bc2e38a23e4e37fbc19cde599eb1beef
jdc011 <jdc011@ucsd.edu>
2018-02-24 13:05:57 -0800

    Floor management logic rework

--- PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/MapsActivity.java
+++ PCMap/PCMap/app/src/main/java/com/example/jeremy/pcmap/MapsActivity.java
@@ -18,6 +18,7 @@ import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import android.text.format.Time;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -52,6 +53,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private Button Floor2;
     private Button Floor3;
     private Button Floor4;
+    private Button[] buttonList = new  Button[4];
+
+    // Button highlights based on current path
+    private boolean[] highlighted = {false, false, false, false};
 
     // Get ready for other layout on click
     //public void init() {
@@ -96,12 +101,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
 
-
         // Create floor buttons
         Floor1 = (Button) findViewById(R.id.floor1);
         Floor2 = (Button) findViewById(R.id.floor2);
         Floor3 = (Button) findViewById(R.id.floor3);
         Floor4 = (Button) findViewById(R.id.floor4);
+
+        // Put buttons in a list
+        buttonList[0] = Floor1;
+        buttonList[1] = Floor2;
+        buttonList[2] = Floor3;
+        buttonList[3] = Floor4;
 
         // Default floor
         if(this.currentFloor == this.floors[0]) {
@@ -118,6 +128,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+        // List of floor buttons
+        final Button[] floorButtons = {Floor1, Floor2, Floor3, Floor4};
+
         // Autocomplete feature in search bar
         final Constants con = new Constants();
         final AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
@@ -131,16 +144,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Default remaining true highlights to false
+                for(int i = 0; i < highlighted.length; i++) {
+                    highlighted[i] = false;
+                }
+
+                // Default floor button colors
+                for(int i = 0; i < floorButtons.length; i++) {
+                    if(floorButtons[i].getCurrentTextColor() == Color.GREEN) {
+                        floorButtons[i].setTextColor(Color.BLACK);
+
+                        if(i+1 == currentFloor) {
+                            floorButtons[i].setTextColor(Color.RED);
+                        }
+                    }
+                }
+
                 //get string from search bar
                 String place = autoCompleteTextView.getText().toString().toLowerCase();
 
                 if (con.toEnum(place) != null) {
+                    // Show floor 1 starting
+                    // Set button colors
+                    currentFloor = floors[0];
+                    Floor1.setTextColor(Color.RED);
+                    Floor2.setTextColor(Color.BLACK);
+                    Floor3.setTextColor(Color.BLACK);
+                    Floor4.setTextColor(Color.BLACK);
+
+                    // Draw current path if destination is searched
+                    if (dest != null && dest.size() != 0)
+                        drawPath(dest.toArray(new PlaceName[0]), currentFloor);
+
+                    // Show current floor plan
+                    showFloor(currentFloor);
+
+                    // Set path highlight colors
+                    for(int i = 0; i < floors.length; i++) {
+                        if(highlighted[i] && i+1 != currentFloor){
+                            buttonList[i].setTextColor(Color.GREEN);
+                        }
+                    }
+
                     //turn string into enum to be used
                     PlaceName thePlace = con.toEnum(place);
                     dest = con.getPath(thePlace);
-                    //need pop up message if user input place that does not exists
                     drawPath(dest.toArray(new PlaceName[0]), currentFloor);
                     showFloor(currentFloor);
+
+                    // Set button colors for available paths
+                    for(int i = 0; i < floors.length; i++) {
+                        if(con.FLOORS.get(thePlace) == floors[i]) {
+                            if(i+1 != currentFloor) {
+                                floorButtons[i].setTextColor(Color.GREEN);
+                            }
+
+                            highlighted[i] = true;
+                        }
+                    }
                 }
 
                 // error case
@@ -171,9 +232,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     autoCompleteTextView.setText("");
                     return;
                 }
+
+                // Clear objects being displayed
                 autoCompleteTextView.setText("");
                 dest.clear();
                 theLine.setVisible(false);
+
+                // Default remaining true highlights to false
+                for(int i = 0; i < highlighted.length; i++) {
+                    highlighted[i] = false;
+
+                    // Set all color back except for current floor
+                    if(floorButtons[i].getCurrentTextColor() == Color.GREEN) {
+                        floorButtons[i].setTextColor(Color.BLACK);
+
+                        if(i+1 == currentFloor) {
+                            floorButtons[i].setTextColor(Color.RED);
+                        }
+                    }
+                }
             }
         });
     }
@@ -260,50 +337,98 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     // Show floor 1
     public void clickFloor1(View view) {
+        // Set button colors
         this.currentFloor = floors[0];
         Floor1.setTextColor(Color.RED);
         Floor2.setTextColor(Color.BLACK);
         Floor3.setTextColor(Color.BLACK);
         Floor4.setTextColor(Color.BLACK);
+
+        // Draw current path if destination is searched
         if (dest != null && dest.size() != 0)
             drawPath(dest.toArray(new PlaceName[0]), currentFloor);
+
+        // Show current floor plan
         showFloor(this.currentFloor);
+
+        // Set path highlight colors
+        for(int i = 0; i < floors.length; i++) {
+            if(highlighted[i] && i+1 != currentFloor){
+                buttonList[i].setTextColor(Color.GREEN);
+            }
+        }
     }
 
     // Show floor 2
     public void clickFloor2(View view) {
+// Set button colors
         this.currentFloor = floors[1];
         Floor1.setTextColor(Color.BLACK);
         Floor2.setTextColor(Color.RED);
         Floor3.setTextColor(Color.BLACK);
         Floor4.setTextColor(Color.BLACK);
+
+        // Draw current path if destination is searched
         if (dest != null && dest.size() != 0)
-            drawPath(dest.toArray(new PlaceName[0]), currentFloor);
+            drawPath(dest.toArray(new PlaceName[1]), currentFloor);
+
+        // Show current floor plan
         showFloor(this.currentFloor);
+
+        // Set path highlight colors
+        for(int i = 0; i < floors.length; i++) {
+            if(highlighted[i] && i+1 != currentFloor){
+                buttonList[i].setTextColor(Color.GREEN);
+            }
+        }
     }
 
     // Show floor 3
     public void clickFloor3(View view) {
+// Set button colors
         this.currentFloor = floors[2];
         Floor1.setTextColor(Color.BLACK);
         Floor2.setTextColor(Color.BLACK);
         Floor3.setTextColor(Color.RED);
         Floor4.setTextColor(Color.BLACK);
+
+        // Draw current path if destination is searched
         if (dest != null && dest.size() != 0)
-            drawPath(dest.toArray(new PlaceName[0]), currentFloor);
+            drawPath(dest.toArray(new PlaceName[2]), currentFloor);
+
+        // Show current floor plan
         showFloor(this.currentFloor);
+
+        // Set path highlight colors
+        for(int i = 0; i < floors.length; i++) {
+            if(highlighted[i] && i+1 != currentFloor){
+                buttonList[i].setTextColor(Color.GREEN);
+            }
+        }
     }
 
     // Show floor 4
     public void clickFloor4(View view) {
+// Set button colors
         this.currentFloor = floors[3];
         Floor1.setTextColor(Color.BLACK);
         Floor2.setTextColor(Color.BLACK);
         Floor3.setTextColor(Color.BLACK);
         Floor4.setTextColor(Color.RED);
+
+        // Draw current path if destination is searched
         if (dest != null && dest.size() != 0)
-            drawPath(dest.toArray(new PlaceName[0]), currentFloor);
+            drawPath(dest.toArray(new PlaceName[3]), currentFloor);
+
+        // Show current floor plan
         showFloor(this.currentFloor);
+
+        // Set path highlight colors
+        for(int i = 0; i < floors.length; i++) {
+            if(highlighted[i] && i+1 != currentFloor){
+                buttonList[i].setTextColor(Color.GREEN);
+            }
+        }
     }
 
     // Return home
